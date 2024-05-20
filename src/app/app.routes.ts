354f5bdec7a02bc87f63e07080e9e3005de46d6c
Henrique <tomiattihtm@gmail.com>
2024-05-20 15:50:42 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignupComponent
+    },
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard]
     }
 ];
